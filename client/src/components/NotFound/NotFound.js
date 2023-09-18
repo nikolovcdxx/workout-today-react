cb586df5ad69e9879ea6d314cc8886b264362123
nikolovcdxx <nikolovcdxx@gmail.com>
2023-09-18 20:00:34 +0300

    Not Found page updated

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -1,5 +1,17 @@
 import './notFound.css';
 
 export default function NotFound() {
-    return <h2 className='notFound'>404 NOT FOUND</h2>;
+  return (
+    <div className="not-found">
+      <div className="not-found-container">
+        <div className="track">
+          <div className="runner" />
+        </div>
+        <p className="not-found-message">404</p>
+        <p className="sub-message">
+          Hitting a Dead End? Your Fitness Journey Doesn't Have to!
+        </p>
+      </div>
+    </div>
+  );
 }
